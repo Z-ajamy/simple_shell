5234cc02703214f77d294b3c3d2501c65c25ca42
Z-ajamy <abdorahman0283@gmail.com>
2025-05-02 17:11:43 +0300

    0001

--- simple_shell.c
+++ simple_shell.c
@@ -1,110 +1,60 @@
-#include "simple_shell.h"
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#include"simple_shell.h"
 
-char *find_command(char *command);
-
-int main(void)
+int main()
 {
     char *line = NULL;
-    size_t len = 0;
+    size_t len;
     ssize_t num_chars;
     pid_t pid;
-    int status;
+    int ex, a = 98;
     char **argv = NULL;
-    char *path_cmd = NULL;
+    int flagatty = 1;
 
     while (1)
     {
-        printf("($) ");
+        flagatty = isatty(STDIN_FILENO);
+        if (flagatty)
+            printf("%s ", "($)");
+        
+        len = 0;
         num_chars = getline(&line, &len, stdin);
-        if (num_chars == -1)
+        if (num_chars < 0)
         {
-            printf("\n");
+            if (flagatty != 0)
+                printf("\n");
             break;
         }
-
         if (line[0] == '\n')
             continue;
-
+        
         line[num_chars - 1] = '\0';
-
-        argv = malloc(2 * sizeof(char *));
-        if (!argv)
-        {
-            perror("malloc");
-            continue;
-        }
-
+        argv = (char **)malloc(2 * sizeof(char *));
         argv[0] = line;
         argv[1] = NULL;
-
-        path_cmd = find_command(line);
-        if (!path_cmd)
-        {
-            fprintf(stderr, "Command not found: %s\n", line);
-            free(argv);
-            continue;
-        }
-
         pid = fork();
-        if (pid < 0)
+
+        if (pid > 0)
         {
-            perror("fork");
+            wait(&a);
         }
         else if (pid == 0)
         {
-            if (execve(path_cmd, argv, environ) == -1)
+            ex = execve(line, argv, NULL);
+            if (ex == -1)
             {
-                perror("execve");
+                perror("execve faild");
                 exit(EXIT_FAILURE);
             }
+            free(line);
+            free(argv);
+            exit(a);
         }
         else
         {
-            wait(&status);
+            perror("fork faild");
         }
-
-        free(argv);
-        if (path_cmd != line)
-            free(path_cmd);
     }
-
     free(line);
+    free(argv);
     return 0;
-}
-
-/**
- * find_command - يبحث في PATH عن مسار الأمر الكامل
- */
-char *find_command(char *command)
-{
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *dir = strtok(path_copy, ":");
-    char full_path[1024];
-
-    if (access(command, X_OK) == 0)
-    {
-        free(path_copy);
-        return command;  // المسار الكامل تم إدخاله من البداية
-    }
-
-    while (dir)
-    {
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return strdup(full_path);
-        }
-        dir = strtok(NULL, ":");
-    }
-
-    free(path_copy);
-    return NULL;
 }
